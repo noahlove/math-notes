a34ab6eea7bc8ba2896d6dea0726d7243e3c28b0
Noah Love <noah.love@columbia.edu>
2021-05-02 23:26:55 -0700

    Created general outline and added site description

---
+++ website/gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "Modern Analysis Notes",
+        description: "A large collection of Modern Analysis Notes and Helpful descriptions",
+        siteUrl: "https://example.com",
+        author: 'Noah Love',
+        sections: ['Review', 'Real and Complex Number Systems', 'Basic Topology', 'Numerical Sequences and Series'],
+        twitterAccount: 'noahjameslove',
+        githubRepositoryURL: 'https://github.com/noahlove/math-notes'
+      },
+    },
+  ],
+};
